cda80be099418b19529f9ccdbc06101f4136169c
Galah3ad <chepafadox@gmail.com>
2023-11-30 13:38:29 +0100

    Ajout BO/DAO Utilisateur

---
+++ model/BO/Utilisateur.php
@@ -0,0 +1,50 @@
+<?php
+
+class Utilisateur
+{
+
+    private int $idUtilisateur;
+    private string $identifiant;
+    private string $mdp;
+
+
+    public function __construct(int $idUtilisateur, string $identifiant, string $mdp)
+    {
+        $this->idUtilisateur = $idUtilisateur;
+        $this->identifiant = $identifiant;
+        $this->mdp = $mdp;
+    }
+
+
+    public function getIdUtilisateur()
+    {
+        return $this->idUtilisateur;
+    }
+
+    public function setIdUtilisateur($idUtilisateur)
+    {
+        $this->idUtilisateur = $idUtilisateur;
+    }
+
+    public function getIdentifiant()
+    {
+        return $this->identifiant;
+    }
+
+    public function setIdentifiant($identifiant)
+    {
+        $this->identifiant = $identifiant;
+    }
+
+    public function getmdp()
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp($mdp)
+    {
+        $this->identifiant = $mdp;
+    }
+
+
+}
---
+++ model/DAO/UtilisateurDAO.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class UtilisateurDAO
+{
+    private $connexion;
+
+    public function __construct($connexion)
+    {
+        $this->connexion = $connexion;
+    }
+
+    public function add(Utilisateur $utilisateur)
+    {
+
+    }
+
+
+    public function update(Utilisateur $utilisateur)
+    {
+
+    }
+
+    public function delete($idUtilisateur)
+    {
+
+    }
+
+
+}
