7e62d92992b1ecf93fb9aa134f364db044fd4518
Loris01100 <lolobach01100@gmail.com>
2023-11-30 11:56:23 +0100

    BO/DAO Bilan, Section, Entreprise, typeBilan

---
+++ model/BO/Entreprise.php
@@ -0,0 +1,69 @@
+<?php
+
+class Entreprise
+{
+    private $idEntreprise;
+    private $nomEntreprise;
+    private $adresseEntreprise;
+
+    /**
+     * @param $idEntreprise
+     * @param $nomEntreprise
+     * @param $adresseEntreprise
+     */
+    public function __construct($idEntreprise, $nomEntreprise, $adresseEntreprise)
+    {
+        $this->idEntreprise = $idEntreprise;
+        $this->nomEntreprise = $nomEntreprise;
+        $this->adresseEntreprise = $adresseEntreprise;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdEntreprise()
+    {
+        return $this->idEntreprise;
+    }
+
+    /**
+     * @param mixed $idEntreprise
+     */
+    public function setIdEntreprise($idEntreprise)
+    {
+        $this->idEntreprise = $idEntreprise;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomEntreprise()
+    {
+        return $this->nomEntreprise;
+    }
+
+    /**
+     * @param mixed $nomEntreprise
+     */
+    public function setNomEntreprise($nomEntreprise)
+    {
+        $this->nomEntreprise = $nomEntreprise;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresseEntreprise()
+    {
+        return $this->adresseEntreprise;
+    }
+
+    /**
+     * @param mixed $adresseEntreprise
+     */
+    public function setAdresseEntreprise($adresseEntreprise)
+    {
+        $this->adresseEntreprise = $adresseEntreprise;
+    }
+
+}
